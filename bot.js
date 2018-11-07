481e084e95b1d9040bad5f3fe5d937287a7df75c
ABDULLAH000111122ak1 <43869515+ABDULLAH000111122ak1@users.noreply.github.com>
2018-11-07 06:38:39 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,236 +1,29 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "-";
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Ba River`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
-});
 
-client.on("message", async message => {
-        if(!message.channel.guild) return;
- var prefix= "-";
-        if(message.content.startsWith(prefix + 'server')) {
-        let guild = message.guild
-        let channel = message.channel
-        let guildicon = guild.icon_url
-        let members = guild.memberCount
-        let bots = guild.members.filter(m => m.user.bot).size
-        let humans = members - bots
-        let allchannels = guild.channels.size
-        let textchannels = guild.channels.filter(e => e.type === "text")
-        let voicechannels = guild.channels.filter(e => e.type === "voice")
-          var embed = new Discord.RichEmbed()
-          .setColor("#000000")
-          .setTitle(`معلومات عن السيرفر`)
-          .setDescription(`معلومات عن : ${guild.name}`)
-          .addField("صاحب السيرفر :", `${guild.owner}`, true)
-          .addField("أيدي السيرفر :", `${guild.id}`, true)
-          .addField("موقع السيرفر :", `${guild.region}`, true)
-          .addField("مستوى حماية السيرفر :", `${guild.verificationLevel}`, true)
-          .addField("عدد الرومات الصوتية :", `${voicechannels.size}`, true)
-          .addField("عدد الرومات الكتابية :", `${textchannels.size}`, true)
-          .addField("عدد اعضاء السيرفر :", `${members}`, true)
-          .addField("عدد البوتات :", `${bots}`, true)
-          .addField("عدد الاشخاص :", `${humans}`, true)
-          .addField("عدد رتب السيرفر :", `${guild.roles.size}`, true)
-          .addField(`أيموجيات الخاصة بالسيرفر : (${guild.emojis.size})`, `- ${guild.emojis.array()}`, true)
-          .setFooter(`تم انشاء هذه السيرفر في: ${guild.createdAt}`)
- 
-       message.channel.send({ embed: embed });
- 
-      }
-    });
+ client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 3,
+        maxAge: 10800,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
 
-    const Discord = require("discord.js");
-const client = new Discord.Client();
-client.on('ready', () => {
-  client.user.setGame(`on 0 servers | *help | By Suhaib #4272 .`,'https://www.twitch.tv/v5bz');
-  console.log('---------------');
-  console.log(' Bot Is Online')
-  console.log('---------------')
-});
-client.on('message', message => {
-   let embed = new Discord.RichEmbed()
-
-    let args = message.content.split(' ').slice(1).join(' ');
-     if(!message.channel.guild) return;
-if(message.content.split(' ')[0] == '-bc') {
-         message.react("✔️")
-          let embed = new Discord.RichEmbed()
-    .setColor("#FF00FF")
-    .setThumbnail(message.author.avatarURL)   
-                                      .addField('تم الارسال بواسطة :', "<@" + message.author.id + ">")
-                 message.channel.sendEmbed(embed);
-        message.guild.members.forEach(m => {
-            var bc = new Discord.RichEmbed()
-.addField('**● Sender  :**', `*** → ${message.author.username}#${message.author.discriminator}***`)
-            .addField('***● Server  :***', `*** → ${message.guild.name}***`)               
-    .setColor('#ff0000')
-                 .addField('ّ', args)
-            m.send(``,{embed: bc});
-        });
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرابط : ثلاث ساعات | عدد استخدامات الرابط : 3 **")
+      message.author.sendEmbed(Embed11)
     }
-})
-
-client.on('message', function(message) {
-  if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-          let command = message.content.split(" ")[0];
-      if(message.content.includes('discord.gg')){
-      message.reply (' ')
-         if(!message.channel.guild) return message.reply('** This command only for servers**');
-   message.member.addRole(message.guild.roles.find('name', 'Muted')); 
-  const embed500 = new Discord.RichEmbed()
-    .setTitle(":x: | تمت معاقبتك")
-          .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
-    .addField(`by`,`shyboy_05`)
-          .setColor("c91616")
-          .setThumbnail(`${message.author.avatarURL}`)
-          .setAuthor(message.author.username, message.author.avatarURL) 
-      .setFooter(`${message.guild.name} Server`)
-   message.channel.send(embed500) 
-  
-      
-  }
-  }
-})
-
-const Discord = require("discord.js");
-const client = new Discord.Client();
-client.on('ready', () => {
-  console.log(`Hixxx online`);
-
-});
-//Hixx
-client.on('message', message => {
-if (message.content.split(' ')[0] == '-m')
- message.guild.members.forEach( member => {
-         if (!message.member.hasPermission("CONNECT"))  return;
-member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
-                                                            message.delete();
-});
-});
-
-//جميع الحقوق محفوظه لهيكس وخبز
-client.on("message", message => {
-    var prefix = "-";
- 
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "-")) {
-                         if (!message.member.hasPermission("CONNECT"))  return;
-                            let embed4 = new Discord.RichEmbed()
-             .setDescription("**:white_check_mark: | جاري ارسال البرودكاست**")
-           .addField("مرسل البرودكاست" , message.author)
-          .addField("نص البرودكاست" ,args.join("  "))
-                            .addField("عدد الاعضاء المرسل لهم :busts_in_silhouette:" ,` **[${message.guild.memberCount}]**`,true)
-                                                            .setColor("#008000")
-                                message.channel.sendEmbed(embed4);
-                                                      message.delete();
-                            
-                          }
-});
-
-client.on('message', message => {
-  if (message.content.startsWith("-avatar")) {
-if(!message.channel.guild) return;
-
-      var mentionned = message.mentions.users.first();
-
-  var client;
-    if(mentionned){
-        var client = mentionned;
-    } else {
-        var client = message.author;
-
-    }
-
-      const embed = new Discord.RichEmbed()
-                         .addField('Requested by:', "<@" + message.author.id + ">")
-
-      .setColor(000000)
-      .setFooter("bot name", 'ur bot image link')
-      .setImage(client.avatarURL)
-    message.channel.sendEmbed(embed);
-  }
-});
-
-client.on('message', message => {
-  if (message.content === "-help") {
-  let embed = new Discord.RichEmbed()
-.setAuthor(message.author.username)
-.setFooter(`© SHYBOY_05 ™.`, 'https://images-ext-2.discordapp.net/external/X9SanEG0s7Dtv3krTgy-kod_fj6JRVJ2AG4JryCiiz0/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/375761288518828042/fffa31c797e88cf059dd6db424ff456a.png?width=80&height=80')
-.setColor("#000000")
-.addField("Done | تــــم" , ":envelope: | :sleuth_or_spy::skin-tone-3:شيك علي في الخاص")
-
-
-
-message.channel.sendEmbed(embed);
-}
-});
-
-const fs = require('fs')
-const Canvas = require('canvas')
-const jimp = require('jimp')
-const moment = require('moment')
-const prefix = '-';
-
-client.on('message', message => {
-    
-    let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-    let guild = (client, message).guild;
-     let user = message.mentions.users.first();
-     var men = message.mentions.users.first();
-        var heg;
-        if(men) {
-            heg = men
-        } else {
-            heg = message.author
-        }
-      var mentionned = message.mentions.members.first();
-         var h;
-        if(mentionned) {
-            h = mentionned
-        } else {
-            h = message.member
-        }
-       moment.locale('ar-TN');
-{
-  let args = message.content.split(" ").slice(1);
-if(command == "id") {
-    var Canvas = require('canvas')
-  , Image = new Canvas.Image
-  , canvas = new Canvas(450, 170)
-  , ctx = canvas.getContext('2d');
-  ctx.font = '15px Impact';
-  
-Image.src = canvas.toBuffer();
-
-
-    
-ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
-ctx.fillText(`${moment(heg.createdTimestamp).fromNow()}`,90, 20);
-ctx.fillText(`${moment(h.joinedAt).fromNow()}`,110, 70);
-ctx.fillText(`${heg.username}`,70, 40);
-ctx.fillText(`${h.presence.status}`,50, 70);
-
-
-ctx.beginPath();
-ctx.lineTo(50, 102);
-ctx.stroke();
-
-
-
-message.channel.sendFile(canvas.toBuffer());
-}
-}
-});
+}); 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
